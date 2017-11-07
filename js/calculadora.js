d96b0eec4231dd890fa697c25bbea52178519dd3
Gustavo56 <gustavo.cave@hotmail.com>
2017-11-07 16:04:03 -0700

    calculadora html5 js

---
+++ js/calculadora.js
@@ -0,0 +1,34 @@
+//variable global
+var operador = "";
+
+function numeros(num){
+	if(operador == ""){//operando1 
+		var valorInicial=document.calculadora.operando1.value;
+		if(valorInicial == "0"){
+			document.calculadora.operando1.value = "";
+		}
+
+		//Concatena los valores de num
+		//con los del operando
+		document.calculadora.operando1.value =
+		document.calculadora.operando1.value + num;
+	}else{//Operando2
+		var valorInicial=document.calculadora.operando2.value;
+		if(valorInicial == "0"){
+			document.calculadora.operando2.value = "";
+		}
+
+		//Concatena los valores de num
+		//con los del operando
+		document.calculadora.operando1.value =
+		document.calculadora.operando1.value + num;
+
+
+	}
+
+
+}
+function operadores(ope){
+	operador = ope
+
+}
