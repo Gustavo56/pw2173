60db80c3e13e9d8c8c5008592d90a803fdd492ad
Gustavo56 <gustavo.cave@hotmail.com>
2017-11-23 16:07:47 -0700

    ListadoImpresoPDF

--- listado/detalleusuarios.js
+++ listado/detalleusuarios.js
@@ -2,14 +2,23 @@
 const {BrowserWindow}=require('electron').remote
 const app=require('electron').app;
 const $=require('jquery');
+//Constantes para llamar al PDF
+const ipc=require('electron').ipcRenderer
+const botonPDF=document.getElementById('btnPDF')
+//Activamos el evento click del botón btnPDF 
+botonPDF.addEventListener('click',function(event){
+	botonPDF.style.display="none"
+	ipc.send('print-to-pdf')
 
-require('electron').remote.getGlobal('infoUsuarios').nombre;
-	require('electron').remote.getGlobal('infoUsuarios').genero;
-	require('electron').remote.getGlobal('infoUsuarios').foto;
-	require('electron').remote.getGlobal('infoUsuarios').direccion;
-	require('electron').remote.getGlobal('infoUsuarios').telefono;
+})
+
+var nombre=require('electron').remote.getGlobal('infoUsuarios').nombre;
+var genero=require('electron').remote.getGlobal('infoUsuarios').genero;
+var foto=require('electron').remote.getGlobal('infoUsuarios').foto;
+var direccion=require('electron').remote.getGlobal('infoUsuarios').direccion;
+var telefono=require('electron').remote.getGlobal('infoUsuarios').telefono;
 	$("#idNombre").html(nombre);
 	$("#idGenero").html(genero);
 	$("#idDireccion").html(direccion);
-	$("#idTelefono").html(Telefono);
+	$("#idTelefono").html(telefono);
 	$("#idFoto").attr("src",foto);
