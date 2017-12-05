8ada28671a4f928543529488b9645c27555953ea
Gustavo56 <gustavo.cave@hotmail.com>
2017-12-05 16:04:41 -0700

    Validacion de usuario y menu de opciones

---
+++ proyecto/php/entrada.php
@@ -0,0 +1,37 @@
+<?php
+	include 'conexion.php';
+	function validaEntrada(){
+		$conexion=conectaBD();
+		$respuesta = false;
+		$usuario = $_POST["usuario"];
+		$clave = md5($_POST["clave"]);
+		$consulta = "select ncontrol,nombre, clave from alumnos where ncontrol='".$usuario."' and clave='".$clave."' limit 1";
+		//Recordset = conjunto de registros
+		$resConsulta=mysqli_query($conexion,$consulta);
+		//Cuánto registros trae el RecordSet
+		$nombre="";
+		if(mysqli_num_rows($resConsulta) > 0){
+			$respuesta=true;
+			if($registro=mysqli_fetch_array($resConsulta))
+			{
+				$nombre = $registro["nombre"];
+
+
+			}
+		}
+	     $salida = array('respuesta' => $respuesta,
+	     				'nombre' => $nombre);
+	     print json_encode($salida);
+	}
+
+	$opc=$_POST["opc"];
+	switch ($opc) {
+		case 'valida':
+			validaEntrada();
+			break;
+		
+		default:
+			# code...
+			break;
+	}
+?>
